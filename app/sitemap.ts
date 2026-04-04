840438756183c1d2579ca8a7d7a26050f2ee7ac0
zztccss <zztccss@users.noreply.github.com>
2026-04-04 14:26:51 +0000

    更新sitemap域名为jsq.okxxu.com

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://xo8.de5.net'
-
+  const baseUrl = 'https://jsq.okxxu.com'
+  
   return [
     {
       url: baseUrl,
